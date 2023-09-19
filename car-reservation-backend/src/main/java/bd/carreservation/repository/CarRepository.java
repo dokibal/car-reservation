418eceae86eb03149b3fd45653f0dbcd598a11c1
dokibal <doktor.balazs1@gmail.com>
2023-09-19 09:17:27 +0200

    Move backend to another repo
    -For the easier handling of frontend and backend
    backend is moved to another repository: https://github.com/dokibal/car-reservation-backend

--- car-reservation-backend/src/main/java/bd/carreservation/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package bd.carreservation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import bd.carreservation.model.Car;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-
-}
